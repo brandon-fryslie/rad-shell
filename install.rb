460ea2ba59e42fa20a752c7a02065e022c5c12ba
Brandon Fryslie <brandon@fryslie.com>
2017-01-03 13:21:36 -0700

    Fix install.rb

--- install.rb
+++ install.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
+
 class String
   def colorize(color_code) "\e[#{color_code}m#{self}\e[0m" end
   def bold; colorize(1) end
@@ -32,32 +34,29 @@ end
 
 $options = parse_opts
 
-
-# TODO
-
-# clean:
-# remove ~/.zgen
-# brew uninstall fasd
-# remove ~/.zshrc
+puts "Mode: #{$options[:mode]}".cyan
 
 if $options[:mode] == 'clean'
-  if File.exist?('~/.zshrc')
-    puts "Moving existing ~/.zshrc to ~/.zshrc.rad...".yellow
-    `mv ~/.zshrc ~/.zshrc.rad`
+  if File.exist?("#{ENV['HOME']}/.zshrc")
+    puts "Moving existing #{ENV['HOME']}/.zshrc to #{ENV['HOME']}/.zshrc.rad...".yellow
+    puts `mv #{ENV['HOME']}/.zshrc #{ENV['HOME']}/.zshrc.rad`
   end
 
-  if File.exist?('~/.zshrc.bak')
-    puts "Restoring ~/.zshrc.bak -> ~/.zshrc"
-    `mv ~/.zshrc.bak ~/.zshrc`
+  if File.exist?("#{ENV['HOME']}/.zshrc.bak")
+    puts "Restoring #{ENV['HOME']}/.zshrc.bak -> #{ENV['HOME']}/.zshrc"
+    puts `mv #{ENV['HOME']}/.zshrc.bak #{ENV['HOME']}/.zshrc`
   end
 
   puts "Removing Zgen at #{$options[:zgen_dir]}...".yellow
-  `rm #{$options[:zgen_dir]}`
+  puts `rm -rf #{$options[:zgen_dir]}`
 
-  puts "Uninstalling fasd with 'brew uninstall fasd'...".yellow
-  `brew uninstall fasd`
-
-  puts "Done!".green
+  `which fasd`
+  if $? == 0
+    puts "Uninstalling fasd with 'brew uninstall fasd'...".yellow
+    puts `brew uninstall fasd`
+  else
+    puts "Did not find 'fasd' binary, skipping fasd uninstall...".yellow
+  end
 end
 
 # install:
@@ -66,20 +65,34 @@ end
 # copy .zshrc.example
 # Install fasd
 if $options[:mode] == 'install'
-  if File.exist?('~/.zshrc')
-    puts 'Backing up existing ~/.zshrc -> ~/.zshrc.bak'
-    `mv ~/.zshrc ~/.zshrc.bak`
+  if File.exist?("#{ENV['HOME']}/.zshrc")
+    puts "Backing up existing #{ENV['HOME']}/.zshrc -> #{ENV['HOME']}/.zshrc.bak"
+    puts `mv #{ENV['HOME']}/.zshrc #{ENV['HOME']}/.zshrc.bak`
   end
 
-  if File.exist?('~/.zshrc.rad')
-    puts 'Restoring existing ~/.zshrc.rad -> ~/.zshrc'
-    `mv ~/.zshrc.rad ~/.zshrc`
+  if File.exist?("#{ENV['HOME']}/.zshrc.rad")
+    puts "Restoring existing #{ENV['HOME']}/.zshrc.rad -> #{ENV['HOME']}/.zshrc"
+    puts `mv #{ENV['HOME']}/.zshrc.rad #{ENV['HOME']}/.zshrc`
   else
-    puts 'Copying default .zshrc -> ~/.zshrc'
-    `mv ~/.zshrc.rad ~/.zshrc`
+    puts "Copying default .zshrc -> #{ENV['HOME']}/.zshrc"
+    zshrc_path = "#{File.expand_path(File.dirname(__FILE__))}/.zshrc.example"
+    puts `cp #{zshrc_path} #{ENV['HOME']}/.zshrc`
   end
 
-  puts "Cloning Zgen into #{$options[:zgen_dir]}...".yellow
-  `git clone https://github.com/tarjoilija/zgen.git "#{$options[:zgen_dir]}"`
+  if !File.exist?("#{$options[:zgen_dir]}")
+    puts "Cloning Zgen into #{$options[:zgen_dir]}...".yellow
+    puts `git clone https://github.com/tarjoilija/zgen.git "#{$options[:zgen_dir]}"`
+  else
+    puts "#{$options[:zgen_dir]} exists, skipping clone...".yellow
+  end
 
+  `which fasd`
+  if $? == 0
+    puts "Found 'fasd' binary, skipping fasd installation...".yellow
+  else
+    puts "Doing 'brew install fasd'...".yellow
+    puts `brew install fasd`
+  end
 end
+
+puts "Done!".green
