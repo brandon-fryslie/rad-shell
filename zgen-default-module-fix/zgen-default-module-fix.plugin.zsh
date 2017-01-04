9aaed44fc984b0867c80a9ac22b881a3d036fde9
Brandon Fryslie <bfryslie@rallydev.com>
2017-01-03 22:40:35 -0700

    fix: add zgen-default-module-fix plugin to patch up a problem caused by zgen :(

---
+++ zgen-default-module-fix/zgen-default-module-fix.plugin.zsh
@@ -0,0 +1,9 @@
+# We need this to tell prezto we have loaded its default modules
+# Other modules may rely on this being set, but due to the way we load these
+# plugins with Zgen to maintain the correct ordering, we need to patch this up
+# ourselves
+# See: https://github.com/tarjoilija/zgen/issues/74
+for module in "environment" "terminal" "editor" "history" "directory" "spectrum" "utility" "completion" "prompt" "fasd"; do
+  echo "Initing module $module"
+  zstyle ":prezto:module:$module" loaded 'yes'
+done
