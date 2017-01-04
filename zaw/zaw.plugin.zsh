fad1d6549d824b2df2ed108f16f2492a668bb1c8
Brandon Fryslie <bfryslie@mstanley-mbr.f4tech.com>
2017-01-03 21:05:53 -0700

    feat(zaw-plugin): Add better keybindings for zaw

--- zaw/zaw.plugin.zsh
+++ zaw/zaw.plugin.zsh
@@ -1,6 +1,6 @@
 # Configure Zaw
-bindkey '^X' zaw
-bindkey '^R^R' zaw-history
+
+# Visual/Behavioral stuff
 zstyle ':filter-select' max-lines 10
 zstyle ':filter-select' max-lines -10
 zstyle ':filter-select' hist-find-no-dups yes
@@ -10,3 +10,20 @@ zstyle ':filter-select' rotate-list yes
 zstyle ':filter-select:highlight' selected fg=cyan,underline
 zstyle ':filter-select:highlight' matched fg=yellow
 zstyle ':filter-select:highlight' title fg=yellow,underline
+
+# Key bindings
+bindkey '^X' zaw
+bindkey '^R^R' zaw-history
+
+# These bindings take effect when using zaw
+
+# accept-line puts the line into the prompt without executing it
+# accept-search executes it immediately
+bindkey -M filterselect '^[^M' accept-line # Escape + Enter
+bindkey -M filterselect '^M' accept-search # Enter
+
+bindkey -M filterselect '^[F' forward-word # Escape + F
+bindkey -M filterselect '^[B' backward-word # Escape + B
+
+bindkey -M filterselect '^[,' beginning-of-history # Escape + <
+bindkey -M filterselect '^[.' end-of-history # Escape + >
