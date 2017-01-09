a364cc642c36c9f1a01b7ba7f6e24f71b4f3249c
Brandon Fryslie <bfryslie@rallydev.com>
2017-01-09 00:43:22 -0700

    Add syntax highlighter configuration.  Add terminal window/tab title config

--- .zgen-setup.zsh
+++ .zgen-setup.zsh
@@ -7,6 +7,18 @@ ZGEN_AUTOLOAD_COMPINIT=1
 # Automatically regenerate zgen configuration when ~/.zgen-setup.zsh changes
 ZGEN_RESET_ON_CHANGE=~/.zgen-setup.zsh
 
+# We need to set some options before we load Prezto for them to take effect
+zstyle ':prezto:*:*' color 'yes'
+zstyle ':completion:*:default' list-colors ''
+
+zstyle ':prezto:module:terminal' auto-title 'yes'
+zstyle ':prezto:module:terminal:window-title' format '%n@%m'
+zstyle ':prezto:module:terminal:tab-title' format '%s'
+
+# Use bash-style word delimiters
+autoload -U select-word-style
+select-word-style bash
+
 source "${HOME}/.zgen/zgen.zsh"
 # if the init scipt doesn't exist
 if ! zgen saved; then
@@ -30,16 +42,20 @@ if ! zgen saved; then
   zgen load zsh-users/zaw
   zgen load zsh-users/zsh-autosuggestions
   zgen load zsh-users/zsh-completions
-  zgen load zsh-users/zsh-syntax-highlighting
 
   zgen load brandon-fryslie/rad-shell git
   zgen load brandon-fryslie/rad-shell nvm-lazy-load
-  zgen load brandon-fryslie/rad-shell shell-customize
   zgen load brandon-fryslie/rad-shell shell-tools
   zgen load brandon-fryslie/rad-shell zaw
 
+  # Load these plugins last
+  zgen load zsh-users/zsh-syntax-highlighting
+  zgen load brandon-fryslie/rad-shell shell-customize
+
   # Theme
   zgen load brandon-fryslie/rad-shell git-taculous-theme/git-taculous
+
+
 
   zgen save
 fi
--- shell-customize/shell-customize.plugin.zsh
+++ shell-customize/shell-customize.plugin.zsh
@@ -4,6 +4,13 @@ zstyle ':completion:*:default' list-colors ''
 export CLICOLOR=1
 export LSCOLORS=gxfxcxdxbxegedabagacad
 
+# Configure the syntax highlighter a little
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+ZSH_HIGHLIGHT_STYLES[globbing]='fg=blue,bold'
+ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=blue,bold'
+ZSH_HIGHLIGHT_STYLES[bracket-error]='fg=red,bold,underline'
+ZSH_HIGHLIGHT_PATTERNS+=('$[a-zA-Z0-9_]#' 'fg=cyan,underline') # Shell variables
+
 # Share history between terminal windows
 setopt INC_APPEND_HISTORY
 setopt SHARE_HISTORY
