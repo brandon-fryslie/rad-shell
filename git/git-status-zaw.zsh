27e92e3bb5f02aa02f86bc29c0f243b68ce09d44
Fryslie <brandon@fryslie.com>
2017-03-02 20:58:24 -0700

    git-zaw: update a lot of stuff

---
+++ git/git-status-zaw.zsh
@@ -0,0 +1,87 @@
+function rad-git-stage {
+    git add "${FILTER_SELECT_SELECTED}" &>/dev/null
+    # echo "staging file: ${FILTER_SELECT_SELECTED}" >> /tmp/zaw.log
+}
+
+zle -N rad-git-stage
+
+function rad-git-unstage {
+    git reset "${FILTER_SELECT_SELECTED}" &>/dev/null
+    # echo "unstaging file: ${FILTER_SELECT_SELECTED}" >> /tmp/zaw.log
+}
+
+zle -N rad-git-unstage
+
+function rad-git-stage-all {
+    git add . &>/dev/null
+    # echo "unstaging file: ${FILTER_SELECT_SELECTED}" >> /tmp/zaw.log
+}
+
+zle -N rad-git-stage-all
+
+function rad-git-unstage-all {
+    git reset . &>/dev/null
+    # echo "unstaging file: ${FILTER_SELECT_SELECTED}" >> /tmp/zaw.log
+}
+
+zle -N rad-git-unstage-all
+
+function zaw-src-rad-git-status-get-candidates() {
+    git rev-parse --git-dir >/dev/null 2>&1
+    [[ $? == 0 ]] || return $?
+    local file_list="$(git status --porcelain)"
+    local file_list_cands="$(echo $file_list | awk '{print $2}')"
+
+    : ${(A)filter_select_candidates::=${(f)${file_list_cands}}}
+
+    : ${(A)filter_select_descriptions::=${${(f)${file_list}}/ M /[modified]        }}
+    : ${(A)filter_select_descriptions::=${${(M)filter_select_descriptions}/AM /[add|modified]    }}
+    : ${(A)filter_select_descriptions::=${${(M)filter_select_descriptions}/MM /[staged|modified] }}
+    : ${(A)filter_select_descriptions::=${${(M)filter_select_descriptions}/M  /[staged]          }}
+    : ${(A)filter_select_descriptions::=${${(M)filter_select_descriptions}/A  /[staged(add)]     }}
+    : ${(A)filter_select_descriptions::=${${(M)filter_select_descriptions}/ D /[deleted]         }}
+    : ${(A)filter_select_descriptions::=${${(M)filter_select_descriptions}/UU /[conflict]        }}
+    : ${(A)filter_select_descriptions::=${${(M)filter_select_descriptions}/AA /[conflict]        }}
+    : ${(A)filter_select_descriptions::=${${(M)filter_select_descriptions}/\?\? /[untracked]       }}
+
+    # echo "rad-git-status getting candidates: ${(j:,:)filter_select_candidates}" >> /tmp/zaw.log
+}
+
+function zaw-src-rad-git-status() {
+    zaw-src-rad-git-status-get-candidates
+
+    while true; do
+        zaw-src-rad-git-status-get-candidates
+        filter-select -f zaw-src-rad-git-status-get-candidates -m -M radgit -d filter_select_descriptions -e select-action -t "select file" -- "${(@)filter_select_candidates}"
+
+        local action="${reply[1]}"
+
+        [[ $action == 'select-action' ]] && break
+    done
+
+    BUFFER="git status"
+    zaw-rad-action "${reply[1]}"
+}
+
+zaw-register-src -n rad-git zaw-src-rad-git
+
+function _rad-git-init-keymap() {
+    integer fd ret
+
+    # be quiet and check filterselect keybind defined
+    exec {fd}>&2 2>/dev/null
+    bindkey -l radgit > /dev/null
+    ret=$?
+    exec 2>&${fd} {fd}>&-
+
+    if (( ret != 0 )); then
+        bindkey -N radgit
+        bindkey -M radgit '^S' rad-git-stage
+        bindkey -M radgit '^D' rad-git-unstage
+        bindkey -M radgit '^[s' rad-git-stage-all
+        bindkey -M radgit '^[d' rad-git-unstage-all
+        bindkey -M radgit '^[d' rad-git-unstage-all
+    fi
+}
+
+_rad-git-init-keymap
--- git/git-zaw.zsh
+++ git/git-zaw.zsh
@@ -1,71 +1,57 @@
 bindkey '^[G' zaw-rad-git
 
-
 # idea for a plugin use:
 # something to do with a branch
 
 function zaw-src-rad-git() {
-    local title="actions"
+    local title="git commands"
     local desc=(\
-        add \
         commit \
-        amend-commit \
-        branches \
         log \
         fetch \
+        status \
     )
     : ${(A)candidates::=${(@)desc}}
     : ${(A)cand_descriptions::=${(@)desc}}
     actions=(\
-        zaw-src-rad-git-execute-action
+        zaw-rad-git-execute-action
     )
     options=(-t "$title")
 }
 
-function zaw-src-rad-git-execute-action() {
+function zaw-rad-git-execute-action() {
     zle -R
-    eval "zaw-src-rad-git-$1"
+    eval "zaw-rad-git-$1"
 }
 
-function zaw-src-rad-git-add() {
-    local cands
-    local file_info="$(git status --porcelain 2>/dev/null)"
-    local file_names=$(echo "$file_info" | awk '{print $2}')
-    : ${(A)zaw_git_add_descriptions::=${(f)file_info}}
-    : ${(A)cands::=${(f)file_names}}
-
-    local reply=()
-    filter-select -m -d zaw_git_add_descriptions -e select-action -t "select file" -- "${(@)cands}"
-    [[ $? -eq 0 ]] || return $?
-
-    [[ ${#reply_marked} -gt 0 ]] && selected_files="${reply_marked[@]}" || selected_files="${reply[2]}"
-
-    BUFFER="git add ${selected_files}"
-    zle "${reply[1]}"
+# This sets global vars: reply, reply_marked
+function zaw-rad-select-options() {
+    local cands=(${(s: :)1})
+    filter-select -m -d cands -e select-action -t "select options" -- "${(@)cands}"
+    [[ $? -eq 0 ]] || zle send-break
 }
 
-function zaw-src-rad-git-commit() {
+function zaw-rad-git-commit() {
     BUFFER="git commit"
-    zle accept-line
+    local last_action="${reply[1]}"
+
+    # Short circuit if they press enter
+    [[ last_action == "accept-line" ]] && { zle accept-line; return }
+
+    zaw-rad-select-options "--all --amend --patch --reset-author"
+
+    # Collect selected options and append to buffer
+    [[ ${#reply_marked} -gt 0 ]] && BUFFER+=" ${(j: :)reply_marked}"
+
+    zaw-rad-action "${reply[1]}"
 }
 
-function zaw-src-rad-git-amend-commit() {
-    BUFFER="git commit --amend"
-    zle accept-line
-}
-
-function zaw-src-rad-git-branches() {
-    zle zaw-git-branches
-}
-
-function zaw-src-rad-git-log() {
+function zaw-rad-git-log() {
     BUFFER="git log"
-    zle accept-line
+    zaw-rad-action "${reply[1]}"
 }
 
-function zaw-src-rad-git-fetch() {
+function zaw-rad-git-fetch() {
     BUFFER="git fetch"
-    zle accept-line
+    zaw-rad-action "${reply[1]}"
 }
-
-zaw-register-src -n rad-git zaw-src-rad-git
