1afc0f34d3e07b5b21e45c58d9298f93a43f9ba6
Fryslie <brandon@fryslie.com>
2017-03-03 19:12:13 -0700

    rad-docker-zaw: conditionally add 'clean' and 'doctor' commands

--- docker/docker-zaw-dhost.zsh
+++ docker/docker-zaw-dhost.zsh
@@ -4,19 +4,18 @@ function zaw-src-rad-docker-dhost() {
     local title="docker hosts"
     : ${(A)candidates::=$(echo "${(v)DHOST_ALIAS_MAP}" | tr ' ' "\n")}
     : ${(A)cand_descriptions::=$(echo "${(k)DHOST_ALIAS_MAP}" | tr ' ' "\n")}
-    actions=(\
-        zaw-rad-docker-dhost-set-dockerhost \
-        # zaw-rad-docker-dhost-clean \
-        # zaw-rad-docker-dhost-doctor \
-        zaw-rad-append-to-buffer \
-    )
-    act_descriptions=(\
-        "set DOCKER_HOST" \
-        # "clean" \
-        # "doctor" \
-        "append name to buffer" \
-    )
-    options=(-t "$title" -m)
+
+    actions=(zaw-rad-docker-dhost-set-dockerhost)
+    command -v 'docker-clean' &> /dev/null && actions+="zaw-rad-docker-dhost-clean"
+    command -v ddoctor &> /dev/null && actions+="zaw-rad-docker-dhost-doctor"
+    actions+=(zaw-rad-append-to-buffer)
+
+    act_descriptions=("set DOCKER_HOST")
+    command -v 'docker-clean' &> /dev/null && act_descriptions+="clean"
+    command -v ddoctor &> /dev/null && act_descriptions+="doctor"
+    act_descriptions+="append name to buffer"
+
+    options=(-t "$title" -m -k)
 }
 
 function zaw-rad-docker-dhost-set-dockerhost() {
