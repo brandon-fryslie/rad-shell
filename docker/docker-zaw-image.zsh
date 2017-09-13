3a2c3b63d1a10a9e9b6c9a011bab8990378e0702
Fryslie <brandon@fryslie.com>
2017-09-13 10:19:26 -0600

    zaw-docker-images: fix commands to use the tag of the specified image

--- docker/docker-zaw-image.zsh
+++ docker/docker-zaw-image.zsh
@@ -23,28 +23,33 @@ function zaw-src-rad-docker-image() {
     options=(-t "$title" -m)
 }
 
+# Helper function to extract $image:$tag from `docker image` output
+function zaw-rad-docker-image-extract-fullname() {
+    echo "$(echo $1 | awk '{print $1}'):$(echo $1 | awk '{print $2}')"
+}
+
 function zaw-rad-docker-image-run() {
-    zaw-rad-buffer-action "docker run -ti $(echo $1 | awk '{print $1}')"
+    zaw-rad-buffer-action "docker run -ti $(zaw-rad-docker-image-extract-fullname $1)"
 }
 
 function zaw-rad-docker-image-inspect() {
-    zaw-rad-buffer-action "docker inspect $(echo $1 | awk '{print $1}')"
+    zaw-rad-buffer-action "docker inspect $(zaw-rad-docker-image-extract-fullname $1)"
 }
 
 function zaw-rad-docker-image-history() {
-    zaw-rad-buffer-action "docker history $(echo $1 | awk '{print $1}')"
+    zaw-rad-buffer-action "docker history $(zaw-rad-docker-image-extract-fullname $1)"
 }
 
 function zaw-rad-docker-image-rmi() {
-    zaw-rad-buffer-action "docker rmi $(echo $1 | awk '{print $1}')"
+    zaw-rad-buffer-action "docker rmi $(zaw-rad-docker-image-extract-fullname $1)"
 }
 
 function zaw-rad-docker-image-append-name-to-buffer() {
-    zaw-rad-buffer-action "$(echo $1 | awk '{print $1}')" accept-search
+    zaw-rad-buffer-action "$(zaw-rad-docker-image-extract-fullname $1)" accept-search
 }
 
 function zaw-rad-docker-image-append-id-to-buffer() {
-    zaw-rad-buffer-action "$(echo $1 | awk '{print $2}')" accept-search
+    zaw-rad-buffer-action "$(echo $1 | awk '{print $3}')" accept-search
 }
 
 zaw-register-src -n rad-docker-image zaw-src-rad-docker-image
