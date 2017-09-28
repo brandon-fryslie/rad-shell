0314eebebb841853a018250e3a4a94d24d4b5d45
Fryslie <brandon@fryslie.com>
2017-09-28 11:26:15 -0600

    docker-zaw-image: allow multiple selections with 'rmi' command (use ctrl+@ to multiselect)

--- docker/docker-zaw-image.zsh
+++ docker/docker-zaw-image.zsh
@@ -29,19 +29,24 @@ function zaw-src-rad-docker-image() {
     options=(-t "$title" -m)
 }
 
-# Helper functions to extract repo, tag, and id from `docker image` output
+# Helper functions
+
+# Extract repo from `docker image` output
 function zaw-rad-docker-image-extract-repo() {
     echo $1 | awk '{print $1}'
 }
 
+# Extract tag from `docker image` output
 function zaw-rad-docker-image-extract-tag() {
     echo $1 | awk '{print $2}'
 }
 
+# Extract id from `docker image` output
 function zaw-rad-docker-image-extract-id() {
     echo $1 | awk '{print $3}'
 }
 
+# Extract unique identifier, either $repo:$tag or $id (if no tag)
 function zaw-rad-docker-image-extract-fullname() {
     local repo tag id
     repo="$(zaw-rad-docker-image-extract-repo $1)"
@@ -50,6 +55,16 @@ function zaw-rad-docker-image-extract-fullname() {
 
     [[ $tag == '<none>' ]] && echo "$id" || echo "$repo:$tag"
 }
+
+# Perform buffer action with multiple images
+function zaw-rad-docker-image-multiselect-action() {
+    local cmd=$1
+    local images=()
+    for i ($selected) images+="$(zaw-rad-docker-image-extract-fullname "$i")"
+    zaw-rad-buffer-action "$cmd ${(j: :)images}"
+}
+
+# Command functions
 
 function zaw-rad-docker-image-run() {
     zaw-rad-buffer-action "docker run -ti $(zaw-rad-docker-image-extract-fullname $1) bash"
@@ -72,7 +87,7 @@ function zaw-rad-docker-image-history() {
 }
 
 function zaw-rad-docker-image-rmi() {
-    zaw-rad-buffer-action "docker rmi $(zaw-rad-docker-image-extract-fullname $1)"
+    zaw-rad-docker-image-multiselect-action 'docker rmi'
 }
 
 function zaw-rad-docker-image-tag() {
