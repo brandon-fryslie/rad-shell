898005acae0fe625383afe6c38d66990583be35b
Brandon Fryslie <brandon.fryslie@workday.com>
2017-12-05 16:11:38 -0700

    Add homebrew plugin that will install and/or modify path to set up homebrew

---
+++ homebrew/homebrew.plugin.zsh
@@ -0,0 +1,13 @@
+# Plugin for ensuring homebrew is installed
+
+install_cmd='/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+brew_bin=/usr/local/bin/brew
+
+if [[ $(uname) == 'darwin' ]]; then
+  if [[ -x $brew_bin ]]; then
+    export PATH="/usr/local/bin:$PATH"
+  else
+    echo "Installing Homebrew with command ${install_cmd}"
+    $install_cmd
+  fi
+fi
