93c5b9e4d0fd472d15243916fb6271a5ffa8321b
Brandon Fryslie <brandon@fryslie.com>
2017-12-20 14:54:44 -0700

    Fix error thrown by nvm-lazy-load module if nvm is not installed

--- nvm-lazy-load/nvm-lazy-load.zsh
+++ nvm-lazy-load/nvm-lazy-load.zsh
@@ -8,31 +8,37 @@
 # Credit goes to user 'sscotth' on reddit
 # https://www.reddit.com/r/node/comments/4tg5jg/lazy_load_nvm_for_faster_shell_start/d5ib9fs/
 
-declare -a NODE_GLOBALS
-NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
-
-NODE_GLOBALS+=("node")
-NODE_GLOBALS+=("nvm")
-
 load_nvm () {
-    export NVM_DIR=~/.nvm
-    zstyle ':nvm-lazy-load' nvm-loaded 'yes'
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+  export NVM_DIR=~/.nvm
+  zstyle ':nvm-lazy-load' nvm-loaded 'yes'
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 }
 
+declare -a NODE_GLOBALS
 setup_node_aliases() {
-    local script
-    for cmd in "${NODE_GLOBALS[@]}"; do
-        script=$(cat <<-EOSCRIPT
-            ${cmd}() {
-                unset -f ${NODE_GLOBALS}
-                zstyle -t ':nvm-lazy-load' nvm-loaded 'yes' || load_nvm
-                ${cmd} \$@
-            }
+  # Do not create stubs for node globals if no versions of node are installed
+  if [[ -d ~/.nvm/versions/node ]]; then
+    NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+    NODE_GLOBALS+=("node")
+  fi
+
+  NODE_GLOBALS+=("nvm")
+
+  local script
+  for cmd in "${NODE_GLOBALS[@]}"; do
+      script=$(cat <<-EOSCRIPT
+          ${cmd}() {
+              unset -f ${NODE_GLOBALS}
+              zstyle -t ':nvm-lazy-load' nvm-loaded 'yes' || load_nvm
+              ${cmd} \$@
+          }
 EOSCRIPT
-    )
-        eval "$script"
-    done
+  )
+      eval "$script"
+  done
 }
 
-setup_node_aliases
+# Do not setup aliases if nvm is not installed
+if [[ -d ~/.nvm ]]; then
+  setup_node_aliases
+fi
