6b936893cc52d02f7e791cd937709b85e7c88192
brandon.fryslie <brandon.fryslie@workday.com>
2023-10-16 23:59:07 -0600

    Add rad-get-editor and rad-get-visual-editor functions

--- init-plugin/init-plugin.plugin.zsh
+++ init-plugin/init-plugin.plugin.zsh
@@ -1,6 +1,14 @@
 # These are some functions to be shared among rad plugins
 # Functions will be prefixed with 'rad-' to avoid any conflicts
 
+rad-get-visual-editor() {
+  echo "${VISUAL:-${EDITOR:-vi}}"
+}
+
+rad-get-editor() {
+  echo "${EDITOR:-vi}"
+}
+
 # rad zstyle functions
 rad-zstyle-set() {
   local context=$1 style=$2 varname=$3
